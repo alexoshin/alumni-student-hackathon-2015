e2f28e5984a4aa28f3762f6cf50070284887de79
Titus Woo <titus.woo@me.com>
2015-08-29 11:12:52 -0400

    Made quick gmApiService for us to do their api stuff in.

---
+++ www/js/gmApiService.js
@@ -0,0 +1,10 @@
+/**
+ * Created by tituswoo on 8/29/15.
+ */
+angular.module('starter.services').factory('gmApiService', function ($http) {
+  return {
+    authenticate: function () {
+      alert('do authentication here');
+    }
+  };
+});
