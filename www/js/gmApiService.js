1c79a9fc90236c4bc982d6cdf1dbf80b8dedd696
Drew Galbraith <dgalbraith33@gmail.com>
2015-08-29 13:24:20 -0400

    First try at GM API Access

--- www/js/gmApiService.js
+++ www/js/gmApiService.js
@@ -4,7 +4,22 @@
 angular.module('starter.services').factory('gmApiService', function ($http) {
   return {
     authenticate: function () {
-      console.info('do authentication here... maybe?');
+      var id = "l7xxb1ace3b4c4454f1a9ec7a30b69c850f6";
+      var secret = "5d41035a5619413d8cebe6eb1c4bd9eb";
+      var url = "https://developer.gm.com/api/v1/oauth/access_token";
+      var headers = {
+        "Authorization": "Basic " + btoa(id + ":" + secret),
+        "Accept": "application/json",
+        "Access-Control-Allow-Origin": "*"
+      }
+
+      var res = $http.get(url, {headers: headers})
+
+      res.then(function (resp) {
+        console.log(resp)
+      }, function (err) {
+        console.error(err)
+      })
     }
   };
 });
