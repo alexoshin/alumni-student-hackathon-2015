e905b925ccba2c862c028dd55ded6bf4cde28ec0
Titus Woo <titus.woo@me.com>
2015-08-29 12:21:53 -0400

    refactored gassed thing

--- www/js/mgfService.js
+++ www/js/mgfService.js
@@ -2,9 +2,10 @@
  * Created by drew on 8/29/15.
  */
 angular.module('starter.services').factory('mgfService', function($http) {
+  var apiKey = 'ooqlwgi6kb';
   return {
     search: function(lat, long) {
-      var url = "http://api.mygasfeed.com/stations/radius/"+lat+"/"+long+"/5/reg/distance/ooqlwgi6kb.json"
+      var url = "http://api.mygasfeed.com/stations/radius/"+lat+"/"+long+"/5/reg/distance/"+apiKey+".json"
       var res = $http.get(url).then(function(resp) {
         console.log('Success', resp);
       }, function(err) {
@@ -12,4 +13,4 @@ angular.module('starter.services').factory('mgfService', function($http) {
       });
     }
   }
-})
+});
